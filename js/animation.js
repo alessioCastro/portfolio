81c978d6a24c68cf21020a0fd519da9801ed1050
alessioCastro <160541955+alessioCastro@users.noreply.github.com>
2025-05-01 22:37:29 -0300

    Update animation.js

--- js/animation.js
+++ js/animation.js
@@ -1,27 +1,12 @@
-addEventListener('load', () => {
-    //document.getElementById('sp').shadowRoot.querySelector("#logo").remove()
-
-    const jaDeuBoasVindas = sessionStorage.getItem('jaDeuBoasVindas');
-    
+function finalizarIntro() {
     const intro = document.getElementById('intro-screen');
-
     const conteudo = document.getElementById('conteudo');
 
-    if (jaDeuBoasVindas) {
-        intro.remove();
-        conteudo.style.opacity = '1';
-        return;
-    }
+    intro.style.opacity = '0';
+    conteudo.style.animation = 'fadeIn 3s ease forwards 1s';
 
-    // Após 5 segundos (tempo da intro)
     setTimeout(() => {
-        intro.style.opacity = '0';
-        conteudo.style.animation = 'fadeIn 3s ease forwards 1s';
-
-        // Remove completamente o intro-screen depois da transição
-        setTimeout(() => {
-            intro.remove();
-            sessionStorage.setItem('jaDeuBoasVindas', true);
-        }, 2000);
-    }, 5000); // tempo total da intro
-})
+        intro.remove();
+        sessionStorage.setItem('jaDeuBoasVindas', true);
+    }, 2000);
+}
