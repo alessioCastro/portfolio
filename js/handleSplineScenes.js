d2a62b8cca36e9a4eb3a8f6d6ffeab8ffdbc2e2b
alessioCastro <160541955+alessioCastro@users.noreply.github.com>
2025-05-07 17:21:46 -0300

    Update handleSplineScenes.js

--- js/handleSplineScenes.js
+++ js/handleSplineScenes.js
@@ -1,31 +1,39 @@
-import { Application } from 'https://unpkg.com/@splinetool/runtime@latest/build/runtime.js';
-  
-const introCanvas = document.getElementById('spline-welcome');
-let app = new Application(introCanvas);
+import { Application } from "https://unpkg.com/@splinetool/runtime@latest/build/runtime.js";
 
-app.load('https://prod.spline.design/DnzAB-6z7jxJE8Nz/scene.splinecode').then(() => {
-    introCanvas.style.animation = 'fadeIn 3s ease forwards 1s';
-    const jaDeuBoasVindas = sessionStorage.getItem('jaDeuBoasVindas');
+const introCanvas = document.getElementById("spline-welcome");
+const appIntro = new Application(introCanvas);
 
-    const intro = document.getElementById('intro-screen');
+appIntro
+  .load("https://prod.spline.design/DnzAB-6z7jxJE8Nz/scene.splinecode")
+  .then(async () => {
+    // The scene is loaded and ready to go!
 
-    const conteudo = document.getElementById('conteudo');
+    introCanvas.style.animation = "fadeIn 3s ease forwards 1s";
+    const jaDeuBoasVindas = sessionStorage.getItem("jaDeuBoasVindas");
+
+    const intro = document.getElementById("intro-screen");
+
+    const conteudo = document.getElementById("conteudo");
 
     if (jaDeuBoasVindas) {
-        intro.remove();
-        conteudo.style.opacity = '1';
-        return;
+      intro.remove();
+      conteudo.style.opacity = "1";
+      return;
     }
 
-    // Após 5 segundos (tempo da intro)
-    setTimeout(finalizarIntro, 5000);
-});
+    // Espera a câmera girar completamente
+    await waitForCameraRotation(0.47853123486335825);
 
-const canvas = document.getElementById('sp');
-app = new Application(canvas);
+    finalizarIntro();
+  });
 
-app.load('https://prod.spline.design/jRgCwa1zLIxSMrBC/scene.splinecode').then(() => {
-    /*app.addEventListener('mouseHover', (e) => {
+const canvas = document.getElementById("sp");
+const appSp = new Application(canvas);
+
+appSp
+  .load("https://prod.spline.design/jRgCwa1zLIxSMrBC/scene.splinecode")
+  .then(() => {
+    /*appSp.addEventListener('mouseHover', (e) => {
         console.log('Evento mouseDown capturado:', e);
         // e.target contém o objeto clicado
         if (e.target && e.target.name === 'Button') {
@@ -38,4 +46,21 @@ app.load('https://prod.spline.design/jRgCwa1zLIxSMrBC/scene.splinecode').then(()
         console.log(e)
         e.preventDefault();
     }, { passive: false })*/
-});
+  });
+
+function waitForCameraRotation(targetY, tolerance = 0.01) {
+  return new Promise((resolve) => {
+    function checkRotation() {
+      const camera = appIntro._camera;
+      const currentY = camera.rotation.y;
+
+      if (Math.abs(currentY - targetY) < tolerance) {
+        resolve();
+      } else {
+        requestAnimationFrame(checkRotation)
+      }
+    }
+    
+    checkRotation();
+  });
+}
